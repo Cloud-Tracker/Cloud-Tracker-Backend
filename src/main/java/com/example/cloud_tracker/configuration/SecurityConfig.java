f1d6aad0febbf6e5f4381386a66cb994c0816c15
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-02-28 17:38:47 +0200

    * ther explaination of the cors lines :  * 1. config.setAllowedOrigins(Collections.singletonList("*")) :  this line is to allow all origins to access the resources  * 2. config.setAllowedMethods(Collections.singletonList("*")) : this line is to allow all methods to access the resources  * 3. config.setAllowCredentials(true) : this line is to allow the credentials to be sent to the server  * 4. config.setAllowedHeaders(Collections.singletonList("*")) : this line is to allow all headers to access the resources  * 5. config.setMaxAge(3600L) : to set the maximum age of the cache so that the browser does not send the preflight request again

--- src/main/java/com/example/cloud_tracker/configuration/SecurityConfig.java
+++ src/main/java/com/example/cloud_tracker/configuration/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -12,12 +17,24 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http.securityContext((context) -> context
+                .requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("*"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
                 .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
-                );
+                        .anyRequest().permitAll());
         return http.build();
     }
-
 
 }
