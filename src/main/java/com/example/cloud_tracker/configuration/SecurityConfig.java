6854077c472442d44c10c1689380b8176188bf02
Aliya Yasser <aliyayasser1@gmail.com>
2024-04-22 01:33:53 +0200

    add security configuration for cost-info endpoints

--- src/main/java/com/example/cloud_tracker/configuration/SecurityConfig.java
+++ src/main/java/com/example/cloud_tracker/configuration/SecurityConfig.java
@@ -57,8 +57,8 @@ public class SecurityConfig {
 //        .csrf((csrf) -> csrf.getClass().equals(CsrfConfig.class));
 
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/", "/error", "/webjars/**", "/index.html", "/signup","/blogs/blog","/blogs","/blogs/blog/**"
-                                , "/signin").permitAll()
+                        .requestMatchers("/", "/error", "/webjars/**", "/index.html", "/signup","/blogs/blog","/blogs","/blogs/blog/**", "/cost-info/upload",
+                               "cost-info/services" , "cost-info/monthes", "/signin").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(oath2 ->{
                     oath2.successHandler((request, response, authentication) -> {
